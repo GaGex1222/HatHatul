1ebb436ca2ab813132542d046e002552ea7b3d21
Gal <gald12123434@gmail.com>
2024-11-01 05:17:31 +0200

    first commit

---
+++ Player.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace PlayerClass
+{
+    public class Player
+    {
+        Random random = new Random();
+        private string name;
+        private List<int> cards = new List<int>();
+        private static string topCard;
+
+        public string Name {  get { return name; } }
+        public static string TopCard { get { return topCard; } set { topCard = value; } }
+
+        public static void clearConsole()
+        {
+            Console.Clear();
+            Console.WriteLine("Top Card: " + TopCard);
+        }
+
+        public Player(string name)
+        {
+            cards.Add(random.Next(1, 11));
+            cards.Add(random.Next(1, 11));
+            cards.Add(random.Next(1, 11));
+            cards.Add(random.Next(1, 11));
+            this.name = name;
+        }
+
+
+
+        public void showPlayerLeftAndRightCards()
+        {
+            Console.WriteLine($"{name}, " + "Here are your left and right Cards\n" + $"1. {cards[0]} 2. ? 3. ? 4. {cards[3]}");
+        }
+        
+        public void Draw()
+        {
+            clearConsole();
+            int randomCard = random.Next(1, 11);
+            Console.WriteLine(name + $", You have drawn the card {randomCard}");
+            Console.WriteLine("Would you like to 'replace' it with on of your current cards or 'drop' it to be the top card?");
+            string choice = Console.ReadLine().ToLower();
+            clearConsole();
+            if (choice == "replace")
+            {
+                Replace(randomCard);
+            }
+        }
+
+        public void Replace(int card)
+        {
+
+            clearConsole();
+            Console.WriteLine("Select the card index in hand you want to replace (1-4)");
+            int cardToReplaceIndex = Convert.ToInt32(Console.ReadLine());
+            int cardToReplace = cards[cardToReplaceIndex - 1];
+            cardToReplace = card; // replacing the card in the players deck
+            TopCard = cardToReplace.ToString(); // setting the top card to be the thrown card from the player's deck
+        }
+
+        public void Reveal()
+        {
+            Console.WriteLine("Reveal cards!");
+        }
+            
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using HatHatul;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace HatHatulGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string addPlayerChoice;
+            List<Player> playerList = new List<Player>();
+            Console.WriteLine("Players counht: " + playerList.Count);
+            Console.WriteLine("Welcome to HatHatul Written in C#!, Press any key to play!");
+            Console.ReadLine();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Add A Player?");
+                addPlayerChoice = Console.ReadLine().ToLower();
+                if (addPlayerChoice == "yes")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter the player name: ");
+                    string playerName = Console.ReadLine();
+                    Player player = new Player(playerName);
+                    playerList.Add(player);
+                    Console.WriteLine($"{playerName} has been added.");
+
+                }
+            } while(addPlayerChoice != "no");
+
+            foreach (Player player in playerList)
+            {
+                Console.Clear();
+                player.showPlayerLeftAndRightCards();
+                Console.ReadLine();
+            }
+
+
+            foreach (Player player in playerList)
+            {
+                string actionChoice;
+                do
+                {
+                    Player.clearConsole();
+                    Console.WriteLine($"Its {player.Name} Turn!, what will you do, Draw OR Reveal?" );
+                    actionChoice = Console.ReadLine().ToLower();
+                    Console.WriteLine($"{player.Name} have chosen {actionChoice}");
+                    if(actionChoice == "draw")
+                    {
+                        player.Draw();
+                    } else if (actionChoice == "reveal")
+                    {
+                        player.Reveal();
+                    }
+
+                } while(actionChoice != "draw" && actionChoice != "reveal");
+            }
+        }
+    }
+}
